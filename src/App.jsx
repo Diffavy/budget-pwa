d86e18e58831846caff8bd3b1a650ae06e9b84f7
Harrun Muhiden <harrunmuhiden@gmail.com>
2026-05-29 18:57:01 +0100

    add: implement corresponding column titles from supabase

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,22 @@ import { supabase } from "./supabaseClient";
 
 function App() {
   const [amount, setAmount] = useState("");
+  const [type, setType] = useState("");
+  const [flowType, setFlowType] = useState("");
+  const [category, setCategory] = useState("");
 
   const handleSubmit = async (e) => {
     e.preventDefault();
     const { error } = await supabase // inserting an amount value to table
       .from("test_budget")
-      .insert([{ amount: parseFloat(amount) }]);
+      .insert([
+        {
+          amount: parseFloat(amount),
+          type: type,
+          flow_type: flowType,
+          category: category || type === "income" ? "income" : "general", // If category is empty and type is not "income", implies expense, so set category to "general"
+        },
+      ]);
 
     if (error) {
       console.log(error);
